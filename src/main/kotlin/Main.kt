a8c08582cf2e13fffd45e75bc413e9404b66e72f
solji <solji.choi89@gmail.com>
2025-02-14 12:40:11 +0900

    step1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 package com.ll
 
 fun main() {
+    println("== 명언 앱 ==")
 
+    while (true) {
+        print("명언) ")
+
+        val input = readlnOrNull()!!.trim() //절대 null일리가 없는 경우 !!
+
+        if (input == "종료")
+            break
+    }
 }

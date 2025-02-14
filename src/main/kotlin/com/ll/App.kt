5a1fc91dd2fb07caab0cfbcbdaf0a575a540ebae
solji <solji.choi89@gmail.com>
2025-02-14 14:46:31 +0900

    step6 ing..

--- src/main/kotlin/com/ll/App.kt
+++ src/main/kotlin/com/ll/App.kt
@@ -11,10 +11,11 @@ class App {
             print("명령) ")
 
             val input = readlnOrNull()!!.trim() //절대 null일리가 없는 경우 !!
+            val rq = Rq(input)
 
-            if (input == "종료")
+            if (rq.action == "종료")
                 break
-            else if (input == "등록") {
+            else if (rq.action == "등록") {
                 print("명언 : ")
                 val content = readlnOrNull()!!.trim()
 
@@ -27,7 +28,7 @@ class App {
                 wiseSayings.add(wiseSaying)
 
                 println("${id}번 명언이 등록되었습니다.")
-            } else if (input == "목록") {
+            } else if (rq.action == "목록") {
                 if (wiseSayings.isEmpty()) {
                     println("등록된 명언이 없습니다.")
                     continue
@@ -39,6 +40,15 @@ class App {
                 wiseSayings.forEach {
                     println("${it.id} / ${it.author} / ${it.content}")
                 }
+            } else if (rq.action == "삭제") {
+                val id = rq.getParamValueAsInt("id", 0)
+
+                if (id == 0) {
+                    println("id를 정확히 입력해주세요.")
+                    continue
+                }
+
+                println("입력된 id : $id")
             }
         }
     }

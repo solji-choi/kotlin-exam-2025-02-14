58f0d7f23194372bfa77b09582486f1bb60e7d8d
solji <solji.choi89@gmail.com>
2025-02-17 10:03:57 +0900

    test runner

--- src/test/kotlin/com/ll/TestRunner.kt
+++ src/test/kotlin/com/ll/TestRunner.kt
@@ -10,35 +10,30 @@ object TestRunner {
     private val originalOut: PrintStream = System.out
 
     fun run(input: String): String {
-        // 표준 입력 리다이렉팅
-        // 키보드 입력 => 문자열 입력
-        System.setIn(
-            ByteArrayInputStream(
-                ("${input.trimIndent()}\n종료")
-                    .toByteArray()
-            )
-        )
+        val formattedInput = input.trimIndent().plus("\n종료")
 
-        // 표준 출력 리다이렉팅
-        // 콘솔 출력 => 문자열 출력
-        val outputStream = ByteArrayOutputStream()
-        val printStream = PrintStream(outputStream)
-        System.setOut(printStream)
+        return ByteArrayOutputStream().use { outputStream ->
+            PrintStream(outputStream).use { printStream ->
+                try {
+                    System.setIn(
+                        ByteArrayInputStream(
+                            formattedInput.toByteArray()
+                        )
+                    )
 
-        App().run()
+                    System.setOut(printStream)
 
-        // 표준 출력 결과를 문자열로 변환
-        val result = outputStream
-            .toString()
-            .trim()
-            .replace(Regex("\\r\\n"), "\n") // 개행문자 차이 표준화
+                    App().run()
+                } finally {
+                    System.setIn(originalIn)
+                    System.setOut(originalOut)
+                }
+            }
 
-        // 다시 표준 입력으로 복구
-        System.setIn(originalIn)
-
-        // 다시 표준 출력으로 복구
-        System.setOut(originalOut)
-
-        return result
+            outputStream
+                .toString()
+                .trim()
+                .replace("\r\n", "\n")
+        }
     }
 }

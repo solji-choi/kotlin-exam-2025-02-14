8cb8a73e8a76d63c4c72c5dcf6ff0800d648c08c
solji <solji.choi89@gmail.com>
2025-02-17 12:19:58 +0900

    file repository use

--- src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/repository/WiseSayingFileRepository.kt
+++ src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/repository/WiseSayingFileRepository.kt
@@ -11,7 +11,7 @@ class WiseSayingFileRepository : WiseSayingRepository {
         }
 
     override fun save(wiseSaying: WiseSaying): WiseSaying {
-        if (wiseSaying.isNew()) wiseSaying.id = loadLastIdAndIncrease()
+        if (wiseSaying.isNew()) wiseSaying.id = genNextId()
 
         saveOnDisk(wiseSaying)
 
@@ -89,9 +89,9 @@ class WiseSayingFileRepository : WiseSayingRepository {
         }
     }
 
-    private fun loadLastIdAndIncrease(): Int {
-        val lastId = loadLastId()
-        saveLastId(lastId + 1)
-        return lastId
+    private fun genNextId(): Int {
+        return (loadLastId() + 1).also {
+            saveLastId(it)
+        }
     }
 }

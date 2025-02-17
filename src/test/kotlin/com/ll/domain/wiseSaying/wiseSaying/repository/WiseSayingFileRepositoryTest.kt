437e19928308a624c73d93f504330a56e37b6c28
solji <solji.choi89@gmail.com>
2025-02-17 12:10:42 +0900

    file repository save last id

--- src/test/kotlin/com/ll/domain/wiseSaying/wiseSaying/repository/WiseSayingFileRepositoryTest.kt
+++ src/test/kotlin/com/ll/domain/wiseSaying/wiseSaying/repository/WiseSayingFileRepositoryTest.kt
@@ -47,4 +47,10 @@ class WiseSayingFileRepositoryTest {
 
         assertThat(foundWiseSaying).isEqualTo(wiseSaying)
     }
+
+    @Test
+    fun `saveLastId, loadLastId`() {
+        wiseSayingRepository.saveLastId(10)
+        assertThat(wiseSayingRepository.loadLastId()).isEqualTo(10)
+    }
 }
